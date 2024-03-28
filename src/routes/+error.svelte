b57193dcc94d8574b6ec92892663653b36382b46
Dinis Rosas <dinisrosas@hotmail.com>
2024-03-28 00:05:34 +0000

    error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,14 @@
+<svelte:head>
+	<title>Buy Monopoly Stickers Marketplace</title>
+</svelte:head>
+
+<section>
+	Click here <a href="https://www.buymonopolygostickers.com">buymonopolygostickers.com</a> if it doesn't
+	redirect automatically
+</section>
+
+<style>
+	section {
+		padding: 1rem;
+	}
+</style>
